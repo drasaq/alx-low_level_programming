73152efa0558ae10dc4156ff9c9bbab7e4403b09
abdulrasaq okeowo <oladapookeowo@gmail.com>
2022-06-05 14:36:58 -0700

    Grit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: print every alphabet in lowercase
+*
+* Return: return every alphabet
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; ++c)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
