38f8aee53b370186fe2c57d3eac28509668f9fd8
abdulrasaq okeowo <oladapookeowo@gmail.com>
2022-06-05 18:49:31 -0700

    Gritty

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
 /**
-* main - entry block
-* @void: no argument
-* Return: 0
+* main - Prints the numbers from 0-9.
+*  (*
+* Return: 0 on success
 */
 int main(void)
 {
-	int i;
-	char x;
-
-	for (i = 0; i < 10; i++)
-		putchar(i + '0');
-	for (x = 'a'; x <= 'f'; x++)
-		putchar(x);
-	putchar('\n');
-	return (0);
+		printf("0123456789\n");
+			return (0);
 }
