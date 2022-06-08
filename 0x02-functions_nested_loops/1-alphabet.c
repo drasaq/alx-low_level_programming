9c8c954718ae6cdb00343787805201b659bd9423
abdulrasaq okeowo <oladapookeowo@gmail.com>
2022-06-08 14:48:28 -0700

    kok

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,15 @@
 #include "main.h"
-
 /**
- *  *  * print_alphabet - check the code
- *   *   *
- *    *    * Return: Always 0.
+ *  * print_alphabet - print alphabet lowercase
+ *   *
+ *    * Return: Always 0.
  */
 void print_alphabet(void)
 {
-int i;
-
-for (i = 'a'; i <= 'z'; i++)
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
-_putchar(i);
+_putchar(alphabet);
 }
 _putchar('\n');
 }
