76cc53dfe21501eec17e7d9cb25e47782b9d4b26
abdulrasaq okeowo <oladapookeowo@gmail.com>
2022-06-08 16:00:44 -0700

    task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,7 @@ void print_times_table(int n)
 int i, j, k;
 
 if (n >= 0 && n <= 15)
-{			
+{
 for (i = 0; i <= n; i++)
 {
 for (j = 0; j <= n; j++)
@@ -25,14 +25,16 @@ _putchar(' ');
 _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
-} else if (k >= 10 && k < 100)
+else if (k >= 10 && k < 100)
+}
 {
 _putchar(',');
 _putchar(' ')
 _putchar(' ');
 _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
-} else if (k >= 100)
+else if (k >= 100)
+}
 {
 _putchar(',');
 _putchar(' ');
@@ -43,5 +45,4 @@ _putchar((k % 10) + '0')
 }
 _putchar('\n');
 }
-}
-}
+
